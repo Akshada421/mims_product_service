78e2872df56d4fad1395909bf327b0b20285ba22
HP <HP@Laptop.mshome.net>
2024-03-17 02:07:47 +0530

    Updated for Host logging config

--- src/main/java/com/mims/product/MimsProductServiceApplication.java
+++ src/main/java/com/mims/product/MimsProductServiceApplication.java
@@ -1,5 +1,8 @@
 package com.mims.product;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,12 +18,12 @@ import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class MimsProductServiceApplication {
-	
+
 	Logger logger = LoggerFactory.getLogger(MimsProductServiceApplication.class);
 
 	@Value("${server.servlet.context-path}")
 	String context_path;
-	
+
 	@Value("${server.port}")
 	String server_port;
 
@@ -30,18 +33,50 @@ public class MimsProductServiceApplication {
 
 	@Bean
 	public OpenAPI springOpenAPI() {
+		String HOST_NAME = "";
+		try {
+			HOST_NAME = getHostname();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return new OpenAPI()
 				.info(new Info().title("mims_product_service").description("APIs for MIMS Product Service")
 						.version("1.0").license(new License().name("MIMS Dev Team").url("http://localhost:9091/")))
 				.externalDocs(new ExternalDocumentation().description("MIMS Product Service Documentation")
-						.url("http://localhost:9091/"));
+						.url(("http://").concat(HOST_NAME).concat(":").concat(server_port).concat(context_path)));
 	}
-	
+
 	@Bean
-	public CommandLineRunner commandLineRunner() {		
-		final String BASE_LOCALHOST_URL = "http://localhost:";
-		return (args) -> logger.info("URL of your API is : {}",
-				BASE_LOCALHOST_URL.concat(server_port).concat(context_path));
+	public CommandLineRunner commandLineRunner() {
+		final String HOST_NAME;
+		final String HTTP_STRING = "http://";
+		try {
+			HOST_NAME = getHostname();
+			return (args) -> logger.info("URL of your API is : {}",
+					HTTP_STRING.concat(HOST_NAME).concat(":").concat(server_port).concat(context_path));
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	public String getHostname() throws UnknownHostException {
+		String hostname = "";
+		String localHost_hostAddress = InetAddress.getLocalHost().getHostAddress();	
+		String loopback_hostname = InetAddress.getLoopbackAddress().getHostName();
+		logger.info("HostAddress :{}", localHost_hostAddress);	
+		logger.info("hostname :{}", loopback_hostname);
+		if (localHost_hostAddress != null) {
+			logger.info("---Setting up the host---");
+			hostname = localHost_hostAddress;
+		} else {
+			logger.info("---Setting up the default host---");
+			hostname = "localhost:";
+		}
+		return hostname;
 	}
 
 }
